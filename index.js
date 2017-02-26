991d7d2b7ab4b64e3d2e6af3025269befc921861
hongfei.shi <hongfei.shi@tarsocial.com>
2017-02-26 16:46:47 +0800

    router start

---
+++ index.js
@@ -0,0 +1,7 @@
+/**
+ * Created by protp on 2017/2/26.
+ */
+import React from 'react';
+import {render} from 'react-dom';
+import App from './modules/App';
+render (<App />, document.getElementById('app'))
---
+++ modules/App.js
@@ -0,0 +1,9 @@
+/**
+ * Created by proto on 2017/2/26.
+ */
+import React from 'react'
+export default React.createClass({
+    render() {
+        return <div>Hello world</div>
+    }
+})
